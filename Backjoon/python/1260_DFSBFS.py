d62ead16b1821a21f51496700f41b006357e81bb
JuHyang <kkss2889@naver.com>
2019-01-07 17:09:05 +0900

    upload

--- Backjoon/python/1260_DFSBFS.py
+++ Backjoon/python/1260_DFSBFS.py
@@ -1,40 +1,67 @@
-def DFS (node_dict, marked, V) :
-    temp_marked = marked.copy()
-    temp_dict = node_dict.copy()
+def DFS (V, list_graph, list_mark, result) : ## 깊이우선 탐색
+    if list_mark[V - 1] == True :
+        return
+    result.append(V)
+    list_mark[V - 1] = True
+    for i in list_graph[V - 1] :
+        DFS (i, list_graph, list_mark, result)
 
-    stack = [V]
-    temp_marked[V] = True
-    while True :
-        now = stack[-1]
-        stack.append(temp_marked[now][0])
+    return result
 
+def BFS (V, list_graph, list_mark, list_inQueue) :
+    result = []
+    queue = [V]
+    list_inQueue[V - 1] = True
 
-    return;
-input_str = input ()
+    while len (queue) != 0:
+        current = queue.pop(0)
+        result.append(current)
+        list_mark[current - 1] = True
+        for i in list_graph[current - 1] :
+            # print (list_mark)
+            # print (list_inQueue)
+            if list_mark[i - 1] == False and list_inQueue[i - 1] == False :
+                # print (queue)
+                queue.append(i)
+                list_inQueue[i - 1] = True
 
-list_input = input_str.split()
+    return result
 
-N = int (list_input[0])
-M = int (list_input[1])
-V = int (list_input[2])
+def inputGraph (N, M) :
+    list_result = []
+    for i in range (0, N) :
+        list_temp = []
+        list_result.append(list_temp)
 
-node_dict = dict()
-marked = [True]
+    for i in range (0, M) :
+        input_str = input ()
+        list_input = input_str.split()
+        list_result[int(list_input[0]) - 1].append(int(list_input[1]))
 
-for i in range (M) :
-    marked.append(False)
+    return list_result
+
+def printResult (result) :
+    result_str = ""
+    for i in result :
+        result_str += str(i) + " "
+
+    print (result_str)
+def main () :
     input_str = input ()
     list_input = input_str.split()
+    N = int (list_input[0])
+    M = int (list_input[1])
+    V = int (list_input[2])
 
-    node = int (list_input[0])
-    nextNode = int (list_input[1])
+    list_mark = []
+    for i in range (N) :
+        list_mark.append(False)
 
-    if node not in node_dict :
-        node_dict[node] = list ()
+    list_graph = inputGraph (N, M)
+    # print (list_graph)
 
-    node_dict[node].append(nextNode)
-    node_dict[node].sort()
+    printResult (DFS (V, list_graph.copy(), list_mark.copy(), []))
 
-print (node_dict)
+    printResult (BFS (V, list_graph.copy(), list_mark.copy(), list_mark.copy()))
 
-DFS (node_dict, marked, V)
+main()
