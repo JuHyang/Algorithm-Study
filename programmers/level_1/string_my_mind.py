dc3b347d6eca3ff77ba482654c4be05677257ee8
JuHyang <kkss2889@naver.com>
2019-07-15 13:48:23 +0900

    2019.07.15

--- programmers/level_1/string_my_mind.py
+++ programmers/level_1/string_my_mind.py
@@ -3,6 +3,19 @@ def solution(strings, n):
     answer.append(strings[0])
 
     for i in strings[1:] :
-        print ("Hello")
+        index = 0
+        while True :
+            if answer[index][n] > i[n] :
+                answer.insert(index, i)
+                break
+            elif answer[index][n] == i[n] :
+                if answer[index] > i :
+                    answer.insert(index, i)
+                    break
+
+            index += 1
+            if index == len (answer) :
+                answer.append(i)
+                break
 
     return answer
