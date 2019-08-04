e5223f8d586e7b7f7ae1f6f96fa8d81041f54f87
JuHyang <kkss2889@naver.com>
2019-08-05 08:08:19 +0900

    2019.08.04

--- programmers/level_2/22_target_number.py
+++ programmers/level_2/22_target_number.py
@@ -1,18 +1,12 @@
 def BFS (target, i, numbers, stack, result) :
-    print ("i : ", i)
-    print ("stack : ", stack)
-    print ("result : ", result)
     if i == len (numbers) - 1 :
         if stack + numbers[i] == target :
             result += 1
         if stack - numbers[i] == target :
             result += 1
-        print (result)
         return result
     result = BFS (target, i + 1, numbers, stack + numbers[i], result)
-    print (result)
     result = BFS (target, i + 1, numbers, stack - numbers[i], result)
-    print (result)
 
     return result
 
@@ -21,10 +15,3 @@ def solution(numbers, target):
     answer = BFS (target, 0, numbers, 0, 0)
 
     return answer
-
-
-numbers= [1,1,1,1,1]
-target = 3
-
-
-solution (numbers, 3)
