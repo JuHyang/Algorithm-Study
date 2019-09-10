1daa42a6f9915aca5eab1f70bc25eb4e3a45c411
JuHyang <kkss2889@naver.com>
2019-09-11 00:34:28 +0900

    2019.09.10

--- programmers/level_3/3_network.py
+++ programmers/level_3/3_network.py
@@ -1,44 +1,21 @@
+visited = [0] * 200
+def dfs (start, computers, n) :
+    visited[start] = 1
+    for i in range (n) :
+        if visited[i] == 0 and computers[start][i] == 1 :
+            dfs (i, computers, n)
 
+def bfs (start, computers, n) :
+    visited[start] = 1
+    for i in range (n) :
+        if visited[i] == 0 and computers[start][i] == 1 :
+            bfs (i, computers, n)
 
 def solution(n, computers):
     answer = 0
-    dict_computer = dict ()
     for i in range (n) :
-        dict_computer[i] = []
-        for j in range  (n) :
-            if i != j :
-                if computers[i][j] == 1 :
-                    if i in dict_computer :
-                        dict_computer[i].append(j)
-
-    queue = []
-    visited = []
-
-    index = 0
-    while len (dict_computer) != 0 :
-        if index in visited :
-            for i in dict_computer :
-                index = i
-                break
-            continue
-        
-        visited.append(index)
-        if len (dict_computer[index]) == 0:
+        if visited[i] == 0 :
             answer += 1
-            del dict_computer[index]
-            continue
-
-        for i in dict_computer[index] :
-            if i not in visited :
-                queue.append(i)
-
-        del dict_computer[index]
-        if len(queue) == 0 :
-            answer += 1
-            for i in dict_computer :
-                index = i
-                break
-            continue
-        index = queue.pop(0)
+            dfs (i, computers, n)
         
     return answer
